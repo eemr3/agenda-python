c27123b3cebb183c74a488c65d3fa587abf47be0
Emerson Moreira <eemr3@yahoo.com.br>
2020-06-15 23:30:16 -0300

    Adicionado funçoes de alteração e exclusão de dados

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from core.models import  Evento
 # Register your models here.
 
 class Evento_admin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento','data_criacao')
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')
     list_filter = ('usuario', 'data_evento', 'titulo',)
 
 admin.site.register(Evento, Evento_admin)
